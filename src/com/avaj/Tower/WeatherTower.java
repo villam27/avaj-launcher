3b50f991bc92b267d8517b0e60728994de80cf06
villam27 <villam2703@protonmail.com>
2023-12-11 12:58:22 +0100

    [Add] WeatherTower

---
+++ src/com/avaj/Tower/WeatherTower.java
@@ -0,0 +1,11 @@
+package com.avaj.Tower;
+
+import com.avaj.coordinates.Coordinates;
+
+public class WeatherTower extends Tower {
+	public String getWeather(Coordinates p_coordinates) {
+		return "";
+	}
+
+	public void changeWeather() {}
+}
