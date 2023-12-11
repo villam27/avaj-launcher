c77cdf9d2357eb57386faba20269666387057c62
villam27 <villam2703@protonmail.com>
2023-12-11 10:56:04 +0100

    [Add] Aircraft child class

---
+++ src/com/avaj/flyable/Baloon.java
@@ -0,0 +1,9 @@
+package com.avaj.flyable;
+
+import com.avaj.coordinates.Coordinates;
+
+public class Baloon extends Aircraft {
+	public Baloon(long p_id, String p_name, Coordinates p_coordinate) {
+		super(p_id, p_name, p_coordinate);
+	}
+}
---
+++ src/com/avaj/flyable/JetPlane.java
@@ -0,0 +1,9 @@
+package com.avaj.flyable;
+
+import com.avaj.coordinates.Coordinates;
+
+public class JetPlane extends Aircraft {
+	public JetPlane(long p_id, String p_name, Coordinates p_coordinate) {
+		super(p_id, p_name, p_coordinate);
+	}
+}
