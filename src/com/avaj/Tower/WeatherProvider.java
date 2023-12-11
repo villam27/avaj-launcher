4a7610bb3b85d62a7525bb7397d7d75bce60a846
villam27 <villam2703@protonmail.com>
2023-12-11 13:03:04 +0100

    [Add] WeatherProvider

---
+++ src/com/avaj/Tower/WeatherProvider.java
@@ -0,0 +1,13 @@
+package com.avaj.Tower;
+
+import com.avaj.coordinates.Coordinates;
+
+public class WeatherProvider {
+	private String[] weather;
+	
+	private WeatherProvider() {}
+
+	public static String getCurrentWeather(Coordinates p_coordinates) {
+		return "";
+	}
+}
