2ddd7a83d1ddd7c1537c8e6347821cc5bbf1359a
villam27 <villam2703@protonmail.com>
2023-12-13 18:57:48 +0100

    [Add] Weather generation

--- src/com/avaj/Tower/WeatherProvider.java
+++ src/com/avaj/Tower/WeatherProvider.java
@@ -3,11 +3,11 @@ package com.avaj.Tower;
 import com.avaj.coordinates.Coordinates;
 
 public class WeatherProvider {
-	private String[] weather;
-	
+	private static String[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
 	private WeatherProvider() {}
 
 	public static String getCurrentWeather(Coordinates p_coordinates) {
-		return "";
+		int position = p_coordinates.getHeight() + p_coordinates.getLatitude() + p_coordinates.getLongitude();
+		return weather[position % 4];
 	}
 }
